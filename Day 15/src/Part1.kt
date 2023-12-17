b9eb09c294e133557fc4190b50f9db6cbb2ecf4f
Heck21 <cavinawe12@gmail.com>
2023-12-17 14:44:29 -0500

    Added Day 15 Part 1

---
+++ Day 15/src/Part1.kt
@@ -0,0 +1,46 @@
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+fun hash(step: CharArray): Int {
+    var current = 0
+
+    for (char in step) {
+        val code = char.code
+        current += code
+        current *= 17
+        current %= 256
+    }
+
+    return current
+}
+
+
+fun calculate(file: Path): Int {
+    var total = 0
+
+    val steps: MutableList<CharArray> = mutableListOf()
+
+    var lines = file.readLines()
+
+    for (line in lines) {
+        lines = line.split(",")
+    }
+
+    for (step in lines) {
+        steps.add(step.toCharArray())
+    }
+
+    for (step in steps) {
+        total += hash(step)
+    }
+
+    return total
+}
+
+
+fun main() {
+    val file = Path("input.txt")
+
+    println(calculate(file))
+}
