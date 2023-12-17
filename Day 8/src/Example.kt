5bf6d0b51cec87feb6347f57996237352f8ad058
Heck21 <cavinawe12@gmail.com>
2023-12-16 22:53:39 -0500

    Added Part 1 solution for Day 8

---
+++ Day 8/src/Example.kt
@@ -0,0 +1,67 @@
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+typealias StringList = MutableList<String>
+
+fun steps(file: Path): Int {
+    var lines = file.readLines()
+    val directions = lines.first().toCharArray()
+
+    val source = "AAA"
+    val destination = "ZZZ"
+
+    var steps = 0
+
+    var found = false
+
+    val map: MutableMap<String, Pair<String, String>> = mutableMapOf()
+
+    lines = lines.drop(2)
+
+    for (line in lines) {
+        val (node, element) = line.split(" = ")
+        val (left, right) = element
+            .removeSurrounding("(", ")")
+            .split(", ")
+
+        map[node] = Pair(left, right)
+    }
+
+    while (!found) {
+        var current = map[source]
+
+        for (i in 0..Int.MAX_VALUE) {
+            when (directions[i % directions.size]) {
+                'L' -> {
+                    steps++
+                    if (current?.first == destination) {
+                        found = true
+                        break
+                    } else {
+                        current = map[current?.first]
+                    }
+                }
+
+                'R' -> {
+                    steps++
+                    if (current?.second == destination) {
+                        found = true
+                        break
+                    } else {
+                        current = map[current?.second]
+                    }
+                }
+            }
+        }
+    }
+
+    return steps
+}
+
+
+fun main() {
+    val file = Path("input.txt")
+
+    println(steps(file))
+}
